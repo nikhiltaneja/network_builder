843e548aa04356720183a36556b989d477897f28
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-02-18 18:54:56 -0700

    Set up postgres and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NetworkBuilder::Application.config.secret_key_base = 'b0e1754e915b5742b29bd7bb8190a2306745e2bf57d6489e66566f165c63d106c81067f6a6095d4cd5561fda62b22f5ff513843c1ba17506bc6cf53c8f4ffc12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NetworkBuilder::Application.config.session_store :cookie_store, key: '_network_builder_session'
