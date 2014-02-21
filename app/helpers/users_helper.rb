fdd4a1bf1698a048e945cd58b684d4bbb19abf49
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-02-21 13:59:52 -0700

    Add view partials to remove logic from views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+
+  def validate_summary(user)
+    if user.summary && !user.summary.empty?
+      render partial: 'users/summary', locals: {user: user}
+    end
+  end
+
+  def validate_jobs(user)
+    if user.jobs
+      render partial: 'users/jobs', locals: {user: user}
+    end
+  end
+
+  def validate_degrees(user)
+    if user.degrees
+      render partial: 'users/degrees', locals: {user: user}
+    end
+  end
 end
