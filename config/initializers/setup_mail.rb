7470ae5a8514de2316696db3a45215725eb1cfee
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-02-26 11:29:55 -0700

    Update initializer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "network-builder.com",
-  :user_name            => "denvergschool",
-  :password             => "g$chooldenver",
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
